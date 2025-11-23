114591d54f987ef8b5ea4a924639da2e6527d514
DavidSM <dasamiarea@gmail.com>
2025-11-23 22:17:39 +0100

    Pequeño cambio updates, JODER

--- local_scripts/windows/update.ps1
+++ local_scripts/windows/update.ps1
@@ -1,6 +1,6 @@
 # ============================================
-# Script: Update-SSH-Quick.ps1
-# Quick version without DISM analysis
+# Script: Actualizar-SSH-Rapido.ps1
+# Versión rápida sin análisis DISM
 # ============================================
 
 Write-Host "Enabling Windows Update service..." -ForegroundColor Cyan
@@ -8,4 +8,9 @@ Set-Service wuauserv -StartupType Automatic
 Start-Service wuauserv
 
 Write-Host "Searching for updates..." -ForegroundColor Cyan
-usoclient StartInteractiveScan
+UsoClient.exe StartInteractiveScan
+
+#Showing update status
+Write-Host "Showing update status..." -ForegroundColor Cyan
+UsoClient.exe ScanInstallWait
+Write-Host "Updates completed." -ForegroundColor Green
